bce43311f3bde33b7e1bd6878e02ebe23a52eab5
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-05-12 16:32:10 +0200

    Clean up the neovim config

--- .config/nvim/examples/init.lua
+++
@@ -1,9 +0,0 @@
--- example file i.e lua/custom/init.lua
-
--- MAPPINGS
-local map = nvchad.map
-
-map("n", "<leader>cc", ":Telescope <CR>")
-map("n", "<leader>q", ":q <CR>")
-
--- require("my autocmds file") or just declare them here
