555eba3b6f55da63c73053b8a4fb11dbd18a3031
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-08-25 10:36:04 +0200

    Add !! and !$ to fish yo 😆

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -1,1 +1,2 @@
 source $HOME/.config/fish/functions/take_skip.fish
+source $HOME/.config/fish/functions/history.fish
---
+++ .config/fish/functions/history.fish
@@ -0,0 +1,28 @@
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
