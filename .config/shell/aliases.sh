0a95329df76c474076ee998ff6438759f5c602d0
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-04-15 04:49:20 +0200

    [update] Add more aliases

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -1,14 +1,12 @@
-alias clear='clear && ufetch'
-alias cls='/usr/bin/clear'
 alias lsc='/usr/bin/ls --color=auto' # Default ls with colors
 # alias ls='logo-ls -Dh' # Modern ls https://github.com/Yash-Handa/logo-ls
 alias jls='jls -lh'
 alias ls='jls'
-alias cmus='cmus-rpc-rs --link &>/dev/null & /bin/cmus'
-alias neofetch='neofetch --kitty /mnt/Data/Personalize/Wallpapers/anime --crop_mode fill --crop_offset center --xoffset 1 --yoffset 1'
+alias neofetch='neofetch --kitty /mnt/Data/Personalize/Wallpapers --crop_mode fill --crop_offset center --xoffset 1 --yoffset 1'
 alias nfe='neofetch'
 
 # navigation
+alias ~='cd ~'
 alias cd..='cd ..'
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -16,6 +14,13 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
+# Show the current path with (jpwd)
+alias .='jpwd'
+
+# Edit something
+alias e="$EDITOR"
+
+alias r='bat'
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
